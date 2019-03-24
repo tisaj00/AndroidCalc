ad78ed3b5b3bc885522b19d159bf327a91d29e77
Josip Tisaj <josip.tisaj00@gmail.com>
2019-03-24 22:15:59 +0100

    Work in android studio

---
+++ src/main/java/tisaj/calculator/MainActivity.java
@@ -0,0 +1,29 @@
+package tisaj.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button0,button1,button2,button3,button4;
+    EditText edt1;
+    float mValueOne, mValueTwo;
+    boolean mAdd,mSub,mMul,mDiv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button0 = (Button) findViewById(R.id.button0);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+
+
+
+
+    }
+}
