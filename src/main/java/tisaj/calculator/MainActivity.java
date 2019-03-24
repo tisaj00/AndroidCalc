3f9429455ec36f5e97eeef082b804083af300bda
Josip Tisaj <josip.tisaj00@gmail.com>
2019-03-24 23:05:20 +0100

    Work in android studio

--- src/main/java/tisaj/calculator/MainActivity.java
+++ src/main/java/tisaj/calculator/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button0,button1,button2,button3,button4;
+    Button button0,button1,button2,button3,button4,buttonAdd;
     EditText edt1;
     float mValueOne, mValueTwo;
     boolean mAdd,mSub,mMul,mDiv;
@@ -21,8 +21,41 @@ public class MainActivity extends AppCompatActivity {
         button0 = (Button) findViewById(R.id.button0);
         button1 = (Button) findViewById(R.id.button1);
         button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        buttonAdd = (Button) findViewById(R.id.buttonAdd);
 
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edt1.setText(edt1.getText()+"1");
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edt1.setText(edt1.getText()+"2");
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edt1.setText(edt1.getText()+"3");
+            }
+        });
 
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (edt1 == null){
+                    edt1.setText("");
+                }else {
+                    mValueOne = Float.parseFloat(edt1.getText() + "");
+                    mAdd = true;
+                    edt1.setText(null);
+                }
+            }
+        });
 
 
     }
