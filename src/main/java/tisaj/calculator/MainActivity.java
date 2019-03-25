0538c3d8e1534f9dcb9525891894ba1b33bfea2d
Josip Tisaj <josip.tisaj00@gmail.com>
2019-03-25 12:25:09 +0100

    Finish AndroidCalc

--- src/main/java/tisaj/calculator/MainActivity.java
+++ src/main/java/tisaj/calculator/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button0,button1,button2,button3,button4,buttonAdd;
+    Button button0,button1,button2,button3,button4,button5,button6,button7,button8,button9,button10,buttonC,buttonAdd,buttonSub,buttonMul,buttonDiv,btnFin;
     EditText edt1;
     float mValueOne, mValueTwo;
     boolean mAdd,mSub,mMul,mDiv;
@@ -18,12 +18,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        edt1 = (EditText) findViewById(R.id.edt1);
         button0 = (Button) findViewById(R.id.button0);
         button1 = (Button) findViewById(R.id.button1);
         button2 = (Button) findViewById(R.id.button2);
         button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+        button5 = (Button) findViewById(R.id.button5);
+        button6 = (Button) findViewById(R.id.button6);
+        button7 = (Button) findViewById(R.id.button7);
+        button8 = (Button) findViewById(R.id.button8);
+        button9 = (Button) findViewById(R.id.button9);
+        button10 = (Button) findViewById(R.id.button10);
         buttonAdd = (Button) findViewById(R.id.buttonAdd);
+        buttonSub = (Button) findViewById(R.id.buttonSub);
+        buttonMul = (Button) findViewById(R.id.buttonMul);
+        buttonDiv = (Button) findViewById(R.id.buttonDiv);
+        buttonC = (Button) findViewById(R.id.buttonC);
+        btnFin = (Button) findViewById(R.id.btnFin);
 
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edt1.setText(edt1.getText()+"0");
+            }
+        });
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,6 +61,55 @@ public class MainActivity extends AppCompatActivity {
                 edt1.setText(edt1.getText()+"3");
             }
         });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edt1.setText(edt1.getText()+"4");
+            }
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edt1.setText(edt1.getText()+"5");
+            }
+        });
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edt1.setText(edt1.getText()+"6");
+            }
+        });
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edt1.setText(edt1.getText()+"7");
+            }
+        });
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edt1.setText(edt1.getText()+"8");
+            }
+        });
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edt1.setText(edt1.getText()+"9");
+            }
+        });
+        button10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edt1.setText(edt1.getText()+".");
+            }
+        });
+        buttonC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edt1.setText("");
+            }
+        });
+
 
         buttonAdd.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,7 +124,60 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        buttonSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(edt1.getText() + "");
+                mSub = true ;
+                edt1.setText(null);
+            }
+        });
 
+        buttonMul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(edt1.getText() + "");
+                mMul = true ;
+                edt1.setText(null);
+            }
+        });
+
+        buttonDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(edt1.getText()+"");
+                mDiv = true ;
+                edt1.setText(null);
+            }
+        });
+        btnFin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueTwo = Float.parseFloat(edt1.getText() + "");
+
+                if (mAdd == true){
+
+                    edt1.setText(mValueOne + mValueTwo +"");
+                    mAdd=false;
+                }
+
+
+                if (mSub == true){
+                    edt1.setText(mValueOne - mValueTwo +"");
+                    mSub=false;
+                }
+
+                if (mMul == true){
+                    edt1.setText(mValueOne * mValueTwo + "");
+                    mMul=false;
+                }
+
+                if (mDiv == true){
+                    edt1.setText(mValueOne / mValueTwo+"");
+                    mDiv=false;
+                }
+            }
+        });
 
     }
 }
